ea7080db84620faef103a8e2e10eb7c4bec63295
collegefoundation <collegekumardas5245@gmail.com>
2024-10-18 14:32:01 +0530

    Create ifCondition.py

---
+++ ifCondition.py
@@ -0,0 +1,15 @@
+# if condition with a logical operators
+num1=(int)(input("Enter a Number "))
+num2=(float)(input("Enter a Other Number "))
+print("______________________________")
+sum=0
+if(num1>num2):
+ sum=num1+num2
+ print("Sum = ",sum)
+ 
+ 
+else:
+    sub=num1-num2
+    print("Subtract =", sub)
+
+print("OK")    
