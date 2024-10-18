c1a5aa36ce9e50c964499b1b5b10e9ac96a0985e
collegefoundation <collegekumardas5245@gmail.com>
2024-10-18 14:29:29 +0530

    Create AreaofTriangle.py

---
+++ AreaofTriangle.py
@@ -0,0 +1,8 @@
+side1=36
+side2=25
+side3=50
+print("side 1: ", side1,"Side 2: ", side2, "Side 3:", side3)
+semiper=(side1+side2+side3)/2
+area=(semiper*(semiper-side1)*(semiper-side2)*(semiper-side3))**0.5
+print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+print("semiper: ",semiper,"area =", area)
