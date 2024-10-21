ba24c518c6850f2c3a7358104d27c2773d6607bc
collegefoundation <collegekumardas5245@gmail.com>
2024-10-21 12:17:40 +0530

    calculate speed.py

---
+++ calculate speed.py
@@ -0,0 +1,7 @@
+#1. Write a program to input distance and time. Calculate the speed.
+distace=input("Please enter Distance(in meters): ")
+time=input("Please enter Time(in seconds): ")
+print("-------------------------------------------")
+speed= distace/time
+print("Speed =", speed)
+
