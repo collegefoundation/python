a208503117ad47de1608af5a87e4d59fa5ab5f28
collegefoundation <collegekumardas5245@gmail.com>
2024-10-21 12:18:13 +0530

    calculate area and perimeter of a circle.py

---
+++ calculate area and perimeter of a circle.py
@@ -0,0 +1,10 @@
+#Write a program to calculate area and perimeter of a circle.
+subject1=input("Please enter fist subject marks : ")
+subject2=input("Please enter second subject marks : ")
+subject3=input("Please enter Third subject marks : ")
+subject4=input("Please enter Fourth subject marks : ")
+subject5=input("Please enter Fifth subject marks : ")
+print("-------------------------------------------")
+total=subject1+subject2+subject3+subject4+subject5
+Avg=total/5
+print("Total marks= ", total, "Average =", Avg)
