4db066a7416346dd743359e10bfc98394ff89ae1
collegefoundation <collegekumardas5245@gmail.com>
2024-10-21 12:20:40 +0530

    calculate volume of a cuboid.py

---
+++ calculate volume of a cuboid.py
@@ -0,0 +1,7 @@
+#write a program to calculate volume of a cuboid.
+length= (float)(input("Enter the length of cuboid: "))
+width= (float)(input("Enter the length of Width: "))
+height= (float)(input("Enter the length of Height: "))
+print("-------------------------------------------------")
+volume=length*width*height
+print("Volume of Cuboid: ", volume)
